6f90b6b32d28223b6b0401f5bb8924e10f94aa46
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-08-16 15:12:47 -0300

    fix

--- script.js
+++ script.js
@@ -1,40 +1,63 @@
-const text      = document.querySelector('#text');
-const voices    = document.querySelector('#voices');
-const btnSend   = document.querySelector('#btnSend');
+const text = document.querySelector('#text');
+const voicesSelect = document.querySelector('#voices');
+const speedInput = document.querySelector('#speed');
+const speedValueSpan = document.querySelector('#speedValue');
+const volumeInput = document.querySelector('#volume');
+const volumeValueSpan = document.querySelector('#volumeValue');
+const btnSend = document.querySelector('#btnSend');
 
-let selectedVoice = 0;
+let voicesList = [];
+let selectedVoiceIndex = 0;
 
-window.speechSynthesis.addEventListener('voiceschanged', () => {
-    let voicesList = window.speechSynthesis.getVoices();
-    for(let i in voicesList){
-        let optonEl = document.createElement('option');
-        optonEl.setAttribute('value', i);
-        optonEl.innerText = voicesList[i].name;
-        voices.appendChild(optonEl);
-    }
-});
-
-btnSend.addEventListener('click', () => {
-    if(text.value !== ''){
-        let voicesList = window.speechSynthesis.getVoices();
-        let ut = new SpeechSynthesisUtterance(text.value);
-        ut.voice = voicesList[selectedVoice];
-        window.speechSynthesis.speak(ut);
-    }
-});
-
-voices.addEventListener('change', () => {
-    selectedVoice = parseInt(voices.value);
-});
-
-function updateStatus() {
-    if(window.speechSynthesis.speaking){
-        voices.setAttribute('disabled', 'disabled');
-        btnSend.setAttribute('disabled', 'disabled');
-    }else{
-        voices.removeAttribute('disabled');
-        btnSend.removeAttribute('disabled');
+function populateVoices() {
+    voicesList = window.speechSynthesis.getVoices();
+    voicesSelect.innerHTML = '';
+    for (let i = 0; i < voicesList.length; i++) {
+        const optionEl = document.createElement('option');
+        optionEl.value = i;
+        optionEl.innerText = voicesList[i].name;
+        voicesSelect.appendChild(optionEl);
     }
 }
 
-setInterval(updateStatus, 100);
+window.speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+btnSend.addEventListener('click', () => {
+    const inputText = text.value.trim();
+    if (inputText !== '') {
+        const utterance = new SpeechSynthesisUtterance(inputText);
+        utterance.voice = voicesList[selectedVoiceIndex];
+        const speedValue = parseFloat(speedInput.value);
+        const volumeValue = parseFloat(volumeInput.value);
+        utterance.rate = speedValue;
+        utterance.volume = volumeValue;
+        utterance.addEventListener('start', () => {
+            voicesSelect.disabled = true;
+            btnSend.disabled = true;
+            speedInput.disabled = true;
+            volumeInput.disabled = true;
+        });
+        utterance.addEventListener('end', () => {
+            voicesSelect.disabled = false;
+            btnSend.disabled = false;
+            speedInput.disabled = false;
+            volumeInput.disabled = false;
+        });
+        window.speechSynthesis.speak(utterance);
+    }
+});
+
+voicesSelect.addEventListener('change', () => {
+    selectedVoiceIndex = parseInt(voicesSelect.value);
+});
+
+speedInput.addEventListener('input', () => {
+    const speedValue = parseFloat(speedInput.value);
+    speedValueSpan.textContent = speedValue.toFixed(2);
+});
+
+volumeInput.addEventListener('input', () => {
+    const volumeValue = parseFloat(volumeInput.value);
+    const convertedVolumeValue = (volumeValue * 100).toFixed(0);
+    volumeValueSpan.textContent = convertedVolumeValue;
+});
